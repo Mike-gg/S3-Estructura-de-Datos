4c91853931e2716992d0beef14a9a4a7587caeef
Miguel Terrazas <miguel.terrazas193@tectijuana.edu.mx>
2020-09-16 16:27:02 -0700

    v4

--- Estructuras_primitivas/Program.cs
+++ Estructuras_primitivas/Program.cs
@@ -9,7 +9,7 @@ namespace Estructuras_primitivas
             Console.Title = "PROGRAMA USANDO ESTRUCTURAS PRIMITIVAS";
             
             int x = 0, p = 0, p2 = 0;
-            double y = 0,r = 0,r2 = 0, x2 = 0;
+            double y = 0,r ,r2 = 0, x2 = 0;
 
             Console.WriteLine("Menú de opciones a realizar");
             Console.WriteLine("1.- Raíz Cuadrada");
@@ -24,7 +24,7 @@ namespace Estructuras_primitivas
             int z = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Ingresa un número: ");
-            x = int.Parse(Console.ReadLine());
+            x2 = double.Parse(Console.ReadLine());
             Console.WriteLine("Ingresa otro número: ");
             y = double.Parse(Console.ReadLine());
 
@@ -36,8 +36,8 @@ namespace Estructuras_primitivas
                 {
                     case 1:
                         Console.WriteLine("Raíz cuadrada");
-                        r = Math.Sqrt(x);
-                        Console.WriteLine("La raíz cuadrada de {0} es: {1}.", x, r);
+                        r = Math.Sqrt(x2);
+                        Console.WriteLine("La raíz cuadrada de {0} es: {1}.", x2, r);
                         r2 = Math.Sqrt(y);
                         Console.WriteLine("La raíz cuadrada de {0} es: {1}.", y, r2);
                     break;
@@ -46,8 +46,8 @@ namespace Estructuras_primitivas
                         Console.WriteLine("Potencias");
                         Console.WriteLine("Ingresa el número entero que será la potencia: ");
                         p = Int32.Parse(Console.ReadLine());
-                        r = Math.Pow(x, p);
-                        Console.WriteLine("{0} elevado a {1} es: {2}.", x, p, r);
+                        r = Math.Pow(x2, p);
+                        Console.WriteLine("{0} elevado a {1} es: {2}.", x2, p, r);
                         Console.WriteLine("Ingresa el número entero que será la potencia: ");
                         p2 = Int32.Parse(Console.ReadLine());
                         r2 = Math.Pow(y, p2);
@@ -56,8 +56,8 @@ namespace Estructuras_primitivas
                 
                     case 3:
                         Console.WriteLine("Valor absoluto");
-                        r = Math.Abs(x);
-                        Console.WriteLine("El valor absoluto de {0} es: {1}.", x, r);
+                        r = Math.Abs(x2);
+                        Console.WriteLine("El valor absoluto de {0} es: {1}.", x2, r);
                         Console.WriteLine("Valor absoluto");
                         r2 = Math.Abs(y);
                         Console.WriteLine("El valor absoluto de {0} es: {1}.", y, r2);
@@ -65,20 +65,20 @@ namespace Estructuras_primitivas
                 
                     case 4:
                         Console.WriteLine("Número máximo");
-                        r = Math.Max(x, y);
-                        Console.WriteLine("El número máximo de {0} y {1} es: {3}.", x, y, r );
+                        r = Math.Max(x2, y);
+                        Console.WriteLine("El número máximo de {0} y {1} es: {2}.", x2, y, r );
                     break;
                 
                     case 5:
                         Console.WriteLine("Número mínimo");
-                        r = Math.Min(x, y);
-                        Console.WriteLine("El número máximo de {0} y {1} es: {3}.", x, y, r );
+                        r = Math.Min(x2, y);
+                        Console.WriteLine("El número máximo de {0} y {1} es: {2}.", x2, y, r );
                     break;
                 
                     case 6:
                         Console.WriteLine("Truncar valor decimal");
-                        Console.WriteLine("Ingresa un numero: ");
-                        x2 = double.Parse(Console.ReadLine());
+                        //Console.WriteLine("Ingresa un numero: ");
+                       // x2 = double.Parse(Console.ReadLine());
                         r = Math.Truncate(x2);
                         Console.WriteLine("{0} truncado es: {1}",x2 , r );
                         r2 = Math.Truncate(y);
@@ -87,8 +87,6 @@ namespace Estructuras_primitivas
                 
                     case 7:
                         Console.WriteLine("Redondear");
-                        Console.WriteLine("Ingresa un numero: ");
-                        x2 = double.Parse(Console.ReadLine());
                         r = Math.Truncate(x2);
                         Console.WriteLine("{0} redondeado es: {1}",x2 , r );
                         r2 = Math.Truncate(y);
