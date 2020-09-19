15b98ca872d8c0ddb1a9d2ce978642d7ac4d72dd
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-09-19 01:35:37 -0700

    v2

---
+++ 4-E_Unidimensionales/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4_E_Unidimensionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Estructuras Unidimensionales";
+            Console.WriteLine("Alumno: Terrazas Rojo Miguel Arturo");
+            Console.WriteLine("Matricula: #19211740");
+            Console.WriteLine();
+            string [,] a;
+            a = new string[6,2];
+
+            //Console.WriteLine("Lista de alumnos ")
+            for(int c = 0; c < 6; c++)
+            {
+                for(int f = 0; f < 2; f++)
+                {
+                    Console.Write("Ingresa el nombre de un estudiante: ");
+                    a[f,c] = Console.ReadLine();
+                    Console.Write("Ingresa su calificación: ");
+                    a[f,c] = Console.ReadLine();
+                    Console.WriteLine();
+                }
+            }
+
+            for(int c = 0; c < 6; c++)
+            {
+                for(int f = 0; f<2; f++)
+                {
+                    Console.Write(" " + a[c,f]);
+                }
+            }
+
+        }
+    }
+}
