c968c704c3ff7b0bc482727d1aa27a291a7c37b4
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-09-22 14:20:08 -0700

    v3

--- 6E_Uso_de_conjuntos/Program.cs
+++ 6E_Uso_de_conjuntos/Program.cs
@@ -21,24 +21,6 @@ namespace _6E_Uso_de_conjuntos
             HashSet<string> E = new HashSet<string>();
             HashSet<string> F = new HashSet<string>(); 
 
-            HashSet<string> Union2 = new HashSet<string>(C);
-            Union2.UnionWith(D);
-            HashSet<string> Inter2 = new HashSet<string>(C);
-            Inter2.IntersectWith(D);
-            HashSet<string> Dif2 = new HashSet<string>(C);
-            Dif2.ExceptWith(D);
-            HashSet<string> DifS2 = new HashSet<string>(C);
-            DifS2.SymmetricExceptWith(D);
-
-            HashSet<string> Union3 = new HashSet<string>(E);
-            Union3.UnionWith(F);
-            HashSet<string> Inter3 = new HashSet<string>(E);
-            Inter3.IntersectWith(F);
-            HashSet<string> Dif3 = new HashSet<string>(E);
-            Dif3.ExceptWith(F);
-            HashSet<string> DifS3 = new HashSet<string>(E);
-            DifS3.SymmetricExceptWith(F);
-
             Console.WriteLine();
 
             Console.Write("¿Cuántos conjuntos desea realizar? ");
@@ -51,19 +33,19 @@ namespace _6E_Uso_de_conjuntos
 
             if(c >= 2 && c <= 6)
             {
-            Console.WriteLine("-----Menú de opciónes-----");
-            Console.WriteLine("1.- Unión de conjuntos");
-            Console.WriteLine("2.- Intersección de conjuntos");
-            Console.WriteLine("3.- Diferencia de conjuntos");
-            Console.WriteLine("4.- Diferencia simétrica");
-            Console.WriteLine("5.- Todas las opciones");
+                Console.WriteLine("-----Menú de opciónes-----");
+                Console.WriteLine("1.- Unión de conjuntos");
+                Console.WriteLine("2.- Intersección de conjuntos");
+                Console.WriteLine("3.- Diferencia de conjuntos");
+                Console.WriteLine("4.- Diferencia simétrica");
+                Console.WriteLine("5.- Todas las opciones");
 
-            Console.WriteLine();
+                Console.WriteLine();
 
-            Console.Write("Elige una operación a realizar: ");
-            int o = Int32.Parse(Console.ReadLine());
+                Console.Write("Elige una operación a realizar: ");
+                int o = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine();
+                Console.WriteLine();
 
                 switch(c)
                 {
@@ -112,73 +94,182 @@ namespace _6E_Uso_de_conjuntos
                         DifS1.SymmetricExceptWith(B);
 
                         switch(o)
-                            {
-                                case(1):
-                                
-                                    Console.Write("Unión de A y B: ");
+                        {
+                            case(1):
+                                Console.Write("Unión de A y B: ");
                                 foreach(string i in Union1)
                                 {
-                                Console.Write( " {0}", i);
+                                    Console.Write( " {0}", i);
                                 }
                                 Console.WriteLine();
                                 break;
                                 
-                                case(2):
-                                    Console.Write("Intersección de A y B: ");
-                                    foreach(string i in Inter1)
-                                    {
+                            case(2):
+                                Console.Write("Intersección de A y B: ");
+                                foreach(string i in Inter1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
-                                break;
-                                case(3):
-                                    Console.Write("Diferencia de A y B: ");
-                                    foreach(string i in Dif1)
-                                    {
+                                }
+                                Console.WriteLine();
+                            break;
+                            case(3):
+                                Console.Write("Diferencia de A y B: ");
+                                foreach(string i in Dif1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
-                                break;
-                                case(4):
-                                    Console.Write("Diferencia Simétrica de A y B: ");
-                                    foreach(string i in DifS1)
-                                    {
+                                }
+                                Console.WriteLine();
+                            break;
+                            case(4):
+                                Console.Write("Diferencia Simétrica de A y B: ");
+                                foreach(string i in DifS1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
-                                break;
-                                case(5):
-                                    Console.Write("Unión de A y B: ");
-                                    foreach(string i in Union1)
-                                    {
+                                }
+                                Console.WriteLine();
+                            break;
+                            case(5):
+                                Console.Write("Unión de A y B: ");
+                                foreach(string i in Union1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
+                                }
+                                Console.WriteLine();
 
-                                    Console.Write("Intersección de A y B: ");
-                                    foreach(string i in Inter1)
-                                    {
+                                Console.Write("Intersección de A y B: ");
+                                foreach(string i in Inter1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
+                                }
+                                Console.WriteLine();
 
-                                    Console.Write("Diferencia de A y B: ");
-                                    foreach(string i in Dif1)
-                                    {
+                                Console.Write("Diferencia de A y B: ");
+                                foreach(string i in Dif1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
+                                }
+                                Console.WriteLine();
 
-                                    Console.Write("Diferencia Simétrica de A y B: ");
-                                    foreach(string i in DifS1)
-                                    {
+                                Console.Write("Diferencia Simétrica de A y B: ");
+                                foreach(string i in DifS1)
+                                {
                                     Console.Write( " {0}", i);
-                                    }
-                                    Console.WriteLine();
-                                break;
-                            }
+                                }
+                                Console.WriteLine();
+                            break;
+                        }
                     break;
                     case(3):
+                        x = 0;
+                        Console.WriteLine("Ingresa los datos del conjunto A: ");
+                        for(int i=0; i<d; i++)
+                        {
+                            Console.Write("[{0}]", x=x+1);
+                            string v1 = Console.ReadLine();
+                            A.Add(v1);
+                        }
+                        Console.WriteLine();
+                        x = 0;
+
+                        Console.WriteLine("Ingresa los datos del conjunto B: ");
+                        for(int i=0; i<d; i++)
+                        {
+                            Console.Write("[{0}]", x=x+1);
+                            string v2 = Console.ReadLine();
+                            B.Add(v2);
+                        }
+                        Console.WriteLine();
+
+                        Console.Write("Conjunto A: ");
+                            foreach(string i in A)
+                            {
+                                Console.Write( " {0}", i);
+                            }
+                        Console.WriteLine();
+
+                        Console.Write("Conjunto B: ");
+                            foreach(string i in B)
+                            {
+                                Console.Write( " {0}", i);
+                            }
+                        Console.WriteLine();
+
+                        HashSet<string> Union2 = new HashSet<string>(A);
+                        Union2.UnionWith(B);
+                        HashSet<string> Union3 = new HashSet<string>(Union2);
+                        Union3.UnionWith(C);
+                        HashSet<string> Inter2 = new HashSet<string>(A);
+                        Inter2.IntersectWith(B);
+                        Inter2.IntersectWith(B);
+                        HashSet<string> Dif2 = new HashSet<string>(A);
+                        Dif2.ExceptWith(B);
+                        HashSet<string> DifS2 = new HashSet<string>(A);
+                        DifS2.SymmetricExceptWith(B);
+
+                        switch(o)
+                        {
+                            case(1):
+                                Console.Write("Unión de A y B: ");
+                                foreach(string i in Union2)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+                                break;
+                                
+                            case(2):
+                                Console.Write("Intersección de A y B: ");
+                                foreach(string i in Inter1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+                            break;
+                            case(3):
+                                Console.Write("Diferencia de A y B: ");
+                                foreach(string i in Dif1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+                            break;
+                            case(4):
+                                Console.Write("Diferencia Simétrica de A y B: ");
+                                foreach(string i in DifS1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+                            break;
+                            case(5):
+                                Console.Write("Unión de A y B: ");
+                                foreach(string i in Union1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+
+                                Console.Write("Intersección de A y B: ");
+                                foreach(string i in Inter1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+
+                                Console.Write("Diferencia de A y B: ");
+                                foreach(string i in Dif1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+
+                                Console.Write("Diferencia Simétrica de A y B: ");
+                                foreach(string i in DifS1)
+                                {
+                                    Console.Write( " {0}", i);
+                                }
+                                Console.WriteLine();
+                        }
                     break;
                     case(4):
                     break;
