fffe8afdca6343ee8b31104c22cec298b2920ed8
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-10-06 12:21:48 -0700

    v1

---
+++ 11_RecursividadD_SumaNumNat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _11_RecursividadD_SumaNumNat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Programa que Suma de números naturales con recursividad directa";
+            Console.WriteLine("Alumno: Terrazas Rojo Miguel Arturo");
+            Console.WriteLine("Matricula: 19211740");
+
+            Console.WriteLine();
+            Program Me = new Program();
+
+            Console.Write("Ingrese un número: ");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese otro número: ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            Me.Suma(X, Y);
+        }
+        void Suma(int X, int Y)
+        {
+            
+
+        }
+    }
+}
