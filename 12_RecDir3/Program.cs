c482ccd9989b4f75e56e2865d92a3d1dc5a4e5fc
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-10-07 13:43:59 -0700

    v1

---
+++ 12_RecDir3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _12_RecDir3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Clear();
+            Console.Title = "Programa que suma números naturales recursivamente";
+            Console.WriteLine("Alumno: Terrazas Rojo Miguel Arturo");
+            Console.WriteLine("Matricula: 19211740");
+
+            Console.WriteLine();
+
+            Console.Write("Ingrese la cantidad de edades que desea guardar: ");
+
+            int [] e = new int [20];
+
+            for(int v = 0; v < 20; v++)
+            {
+                Console.Write("Ingresa una edad: ");
+                e[v] = Convert.ToInt32(Console.ReadLine());
+
+
+            } 
+        }
+    }
+}
