543d356063769d4060c53a79a8cd1592c8435f4d
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-10-15 14:00:51 -0700

    v8

--- U3/1_PilasSimples/Program.cs
+++ U3/1_PilasSimples/Program.cs
@@ -14,83 +14,64 @@ namespace PilasSimples
 
             Console.WriteLine("");
 
-            int [] edades = new int [100];
+            int [] edades = new int [50];
             int top = 0;
-            int mayor;
 
-            for(top = 0; top < 100; top++)
+            Console.WriteLine("Ingreso de edades");
+            Console.Write("¿Deseas agregar una edad? [1] Sí, [2] No ");
+            int o = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            
+            while(o == 1)
             {
-                Console.WriteLine("-----Menu-----");
-                Console.WriteLine("1.- Insertar edad");
-                Console.WriteLine("2.- Mostrar edades");
-                Console.WriteLine("3.- Eliminar edad");
-                Console.WriteLine("0.- Finalizar");
-                
-                Console.WriteLine("");
-
-                Console.Write("Elige una opción: ");
-                int o = Convert.ToInt32(Console.ReadLine());
-
-                switch(o)
-                {
-                    case 1:
-                        Console.Clear();
+                Console.Clear();
                         
-                        Console.Write("Ingresa una edad: ");
-                        int edad = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingresa una edad: ");
+                int edad = Convert.ToInt32(Console.ReadLine());
 
-                        if(top == edades.Length)
-                        {
-                            Console.WriteLine("Pila llena");
-                        }
-                        else
-                        {
-                            edades[top] = edad;
-                            top = top + 1;
-                        }
-                    break;
-
-                    case 2:
-
-                        for(top = 0; top < 5; top++)
-                        {
-                            Console.WriteLine(edades[top]);
-
-                            mayor = edades[top];
-
-                            if(mayor >= edades[top - 1])
-                            {
-                                Console.WriteLine("La edad mayor es: " + mayor);
-                            }
-                            else
-                            {
-
-                            }
-                        }
-                    break;
-
-                    case 3:
-
-                        int d;
-
-                        if(top == -1)
-                        {
-                            Console.WriteLine("Pila Vacía");
-                        }
-                        else
-                        {
-                            top--;
-                            d = edades[top];
-                            edades[top] = 0;
-                            Console.Write("El valor eliminado es: " + d);
-                        }
-                    break;
-
-                    case 0:
-                        top = 100;
+                if(top < 50)
+                {
+                    edades[top] = edad;
+                    top++;
+                }
+                else
+                {
+                    Console.WriteLine("Pila llena");
                     break;
                 }
+
+                Console.Write("¿Ingresará otra edad? [1] Sí, [2] No ");
+                o = Convert.ToInt32(Console.ReadLine());
             }
+
+            Console.Clear();
+
+            Console.Write("¿Imprimir todas las edades? [1] Sí, [2] No ");
+            o = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine();
+
+            if(o == 1)
+            {
+                Console.WriteLine("Todas las edades registradas");
+                for(int x = top -1; x >= 0; x++)
+                {
+                    Console.WriteLine(edades[x]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Presione una tecla para continuar: ");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+
+            Console.Write("¿Eliminar edades registradas? [1] Sí, [2] No ");
+            o = Convert.ToInt32(Console.ReadLine());
+
+
         }
     }
 }
