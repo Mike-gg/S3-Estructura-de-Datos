78c25cbc84518425b7b1a99b6cddb89e10f2ea43
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-10-15 13:09:05 -0700

    v7

--- U3/1_PilasSimples/Program.cs
+++ U3/1_PilasSimples/Program.cs
@@ -7,44 +7,89 @@ namespace PilasSimples
         static void Main(string[] args)
         {
             Console.Clear();
+
             Console.Title = "Programa de pilas estáticas";
             Console.WriteLine("Alumno: Terrazas Rojo Miguel Arturo");
             Console.WriteLine("Matricula: 19211740");
 
             Console.WriteLine("");
 
-            int [] edad = new int [100];
+            int [] edades = new int [100];
             int top = 0;
+            int mayor;
 
-            Console.WriteLine("---------Menu---------");
-            Console.WriteLine("1.- Ingresar edades");
-            Console.WriteLine("2.- Mostrar mayor edad");
-            Console.WriteLine("3.- Mostrar edades");
-            Console.WriteLine("4.- Eliminar edades");
-            Console.WriteLine("5.- Finalizar programa");
-
-            Console.WriteLine("");
-
-            Console.Write("Ingresa la opción que deseas realizar: ");
-            int o = Convert.ToInt32(Console.ReadLine());
-
-            switch(o)
+            for(top = 0; top < 100; top++)
             {
-                case 1:
-                    for(top = 0; top <= 100; top++ )
-                    {
+                Console.WriteLine("-----Menu-----");
+                Console.WriteLine("1.- Insertar edad");
+                Console.WriteLine("2.- Mostrar edades");
+                Console.WriteLine("3.- Eliminar edad");
+                Console.WriteLine("0.- Finalizar");
+                
+                Console.WriteLine("");
+
+                Console.Write("Elige una opción: ");
+                int o = Convert.ToInt32(Console.ReadLine());
+
+                switch(o)
+                {
+                    case 1:
+                        Console.Clear();
+                        
                         Console.Write("Ingresa una edad: ");
-                        edad[top] = Convert.ToInt32(Console.ReadLine());
-                    }
-                break;
-                case 2:
-                break;
-                case 3:
-                break;
-                case 4:
-                break;
-                case 5:
-                break;
+                        int edad = Convert.ToInt32(Console.ReadLine());
+
+                        if(top == edades.Length)
+                        {
+                            Console.WriteLine("Pila llena");
+                        }
+                        else
+                        {
+                            edades[top] = edad;
+                            top = top + 1;
+                        }
+                    break;
+
+                    case 2:
+
+                        for(top = 0; top < 5; top++)
+                        {
+                            Console.WriteLine(edades[top]);
+
+                            mayor = edades[top];
+
+                            if(mayor >= edades[top - 1])
+                            {
+                                Console.WriteLine("La edad mayor es: " + mayor);
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                    break;
+
+                    case 3:
+
+                        int d;
+
+                        if(top == -1)
+                        {
+                            Console.WriteLine("Pila Vacía");
+                        }
+                        else
+                        {
+                            top--;
+                            d = edades[top];
+                            edades[top] = 0;
+                            Console.Write("El valor eliminado es: " + d);
+                        }
+                    break;
+
+                    case 0:
+                        top = 100;
+                    break;
+                }
             }
         }
     }
