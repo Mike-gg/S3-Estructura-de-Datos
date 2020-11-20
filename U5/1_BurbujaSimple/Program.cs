fdf01e3bd02b6f63e92134255d8a95ce80ca190c
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-11-20 00:26:12 -0800

    a2

---
+++ U5/1_BurbujaSimple/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _1_BurbujaSimple
+{
+    class Program
+    {
+        static int opc;
+        static float [] calfs = new float [30];
+        static float c;
+        static int x = 0;
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Title = "Burbuja simple.";
+
+            Console.WriteLine("Alumno: Terrazas Rojo Miguel Arturo");
+            Console.WriteLine("Matricula: 19211740");
+            Console.WriteLine();
+            Console.Write("Presione una tecla para continuar.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Ingresar();
+            Ordenar();
+            Imprimir();
+        }
+        public static void Ingresar()
+        {
+            Console.Clear();
+            for(x = 0; x < calfs.Length; x++)
+            {
+                Console.Write("Ingrese una calificación: ");
+                calfs[x] = float.Parse(Console.ReadLine());
+            }
+        }
+        public static void Ordenar()
+        {
+            float t;
+            for(int a = 1; a < calfs.Length; a++)
+            {
+                for(int b = (calfs.Length-1); b >= a; b--)
+                {
+                    if(calfs[b - 1] > calfs[b])
+                    {
+                        t = calfs[b-1];
+                        calfs[b - 1] = calfs[b];
+                        calfs[b] = t;
+                    }
+                }
+            }
+        }
+        public static void Imprimir()
+        {
+            Console.Clear();
+            Console.WriteLine("Ordenado descendente.");
+            for(int f = 29; f > -1; f--)
+            {
+                Console.Write("[" + calfs[f] + "] ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ U5/3_BOptimizada/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _3_BOptimizada
+{
+    class Program
+    {
+        static double [] sueldos = new double [30];
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Title = "Burbuja optimizada.";
+
+            Console.WriteLine("Alumno: Terrazas Rojo Miguel Arturo");
+            Console.WriteLine("Matricula: 19211740");
+            Console.WriteLine();
+            Console.Write("Presione una tecla para continuar.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Ingresar();
+            Imprimir();
+            Ordenar();
+        }
+        public static void Ingresar()
+        {
+            Console.Clear();
+            for(int x = 0; x < sueldos.Length; x++)
+            {
+                Console.Write("Ingrese un sueldo: ");
+                sueldos[x] = double.Parse(Console.ReadLine());
+            }
+        }
+        public static void Ordenar()
+        {
+            int i = 1; string Ordenado = "si"; double auxi;
+            do
+            {
+                i++;
+                Ordenado = "si";
+                for(int j = 0; j < (sueldos.Length-1); j++)
+                {
+                    if(sueldos[j] > sueldos[j+1])
+                    {
+                    Ordenado = "No";
+                    auxi = sueldos[j];
+                    sueldos[j] = sueldos[j+1];
+                    sueldos[j+1] = auxi;
+                    }
+                }
+                
+            } while (i < sueldos.Length && Ordenado != "si");
+
+            Console.Clear();
+
+            Console.WriteLine("Ordenado descendente.");
+            for(int f = 29; f > -1; f--)
+            {
+                Console.Write("[" + sueldos[f] + "] ");
+            }
+            Console.ReadKey();
+        }
+        public static void Imprimir()
+        {
+            Console.Clear();
+            Console.WriteLine("Desordenados.");
+            for(int f = 0; f < sueldos.Length; f++)
+            {
+                Console.Write("[" + sueldos[f] + "] ");
+            }
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
