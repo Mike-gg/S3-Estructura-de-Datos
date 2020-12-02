6cdad0a74ef00efe3e3c8311cd3fc9213fca98b6
Miguel Terrazas <60365395+Mike-gg@users.noreply.github.com>
2020-12-02 13:54:33 -0800

    2/12/2020

--- U5/8_Metodo2/obj/Debug/netcoreapp3.1/8_Metodo2.AssemblyInfo.cs
+++ U5/8_Metodo2/obj/Debug/netcoreapp3.1/8_Metodo2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ShellSort")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("8_Metodo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ShellSort")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ShellSort")]
+[assembly: System.Reflection.AssemblyProductAttribute("8_Metodo2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("8_Metodo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
